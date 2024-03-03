37f21f06337648720910b71ae3c4f9ff191af6eb
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-02 19:17:17 -0500

    tried this

--- shittest.py
+++ shittest.py
@@ -3,7 +3,14 @@ Problem Statement
 This program will compute an invoice based on values entered by the user. The invoice computes a
 total (Gross) bill amount based on the number of hours invoiced and the hourly rate (the gross amount
 is the product of those two values).
+
+
 The user provides a discount code of 1, 2, or 3. 1 is 10% discount, 2 is 12% discount, 3 is 15% discount.
 Any other value results in a discount of 0.0.
 An invoice must be printed to the screen.
 """
+
+hr_invoiced = (float(input("Enter hours invoiced")))  # takes string from user and ensures it's a float
+hr_rate = (float(input('Enter the hourly rate')))  # takes string from the user; transforms it into a float
+
+print(hr_invoiced)
