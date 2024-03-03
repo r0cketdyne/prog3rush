e0379ee88bee783cc07504675c83b6768abec0be
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-02 20:59:24 -0500

    final ish

--- shittest.py
+++ shittest.py
@@ -1,6 +1,7 @@
-#cleaned everything up.
+
 
 while True:
+#we will implement a while loop to keep prompting the usr for input until they choose not to continue.
     hr_invoiced = float(input("Please enter hours invoiced: "))
     hr_rate = float(input('Please enter the hourly rate: '))
     discount = float(input("Please enter your discount code (1, 2, 3): "))
@@ -8,23 +9,27 @@ while True:
     product = hr_invoiced * hr_rate
 
     if discount == 1:
-        product *= 0.10  # 10% discount
+        product *= 0.10
+# 10% discount
     elif discount == 2:
-        product *= 0.12  # 12% discount
+        product *= 0.12
+ # 12% discount
     elif discount == 3:
-        product *= 0.15  # 15% discount
+        product *= 0.15
+# 15% discount
     elif discount == 0:
         print("The gross bill amount is ", product)
     else:
         print("Invalid discount code")
 
-    # Calculate and print the gross bill amount
     gross_amount = (hr_invoiced * hr_rate) - product
+# Calculate and print the gross bill amount
     print(f"The gross bill amount is {gross_amount}")
 
-    # Ask the user if they want to continue
     user_input = input("Do you want to enter another set of data? (Y/N): ")
+# Ask the user if they want to continue
     if user_input.lower() != 'y':
-        break  # Exit the loop if the user enters anything other than 'Y' or 'y'
+        break
+# Exit the loop if the user enters anything other than 'Y' or 'y'
 
 
