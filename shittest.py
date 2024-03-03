d55232a3a47eaba5842aa4b604e68a6703f2a61e
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-02 19:00:53 -0500

    emacs is lit

---
+++ shittest.py
@@ -0,0 +1,9 @@
+"""
+Problem Statement
+This program will compute an invoice based on values entered by the user. The invoice computes a
+total (Gross) bill amount based on the number of hours invoiced and the hourly rate (the gross amount
+is the product of those two values).
+The user provides a discount code of 1, 2, or 3. 1 is 10% discount, 2 is 12% discount, 3 is 15% discount.
+Any other value results in a discount of 0.0.
+An invoice must be printed to the screen.
+"""
